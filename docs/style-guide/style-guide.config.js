d42e3b24506d3f5aae7bb93cb9d3882cdc6f837f
Stu Robson <stu.robson@embl.de>
2019-08-06 11:00:34 +0100

    more docs

--- docs/style-guide/style-guide.config.js
+++ docs/style-guide/style-guide.config.js
@@ -4,6 +4,12 @@ const path = require('path');
 
 module.exports = {
   context: {
-    colors: require(path.join(process.cwd(), 'Design-Tokens/dist/documentation/colors.ios.json'))
+    colors: require(path.join(process.cwd(), 'Design-Tokens/dist/documentation/colors.ios.json')),
+    typography: require(path.join(process.cwd(), 'Design-Tokens/dist/documentation/typography.ios.json')),
+    family: require(path.join(process.cwd(), 'Design-Tokens/dist/documentation/font-family.ios.json')),
+    radius: require(path.join(process.cwd(), 'Design-Tokens/dist/documentation/border-radius.ios.json')),
+    duration: require(path.join(process.cwd(), 'Design-Tokens/dist/documentation/duration.ios.json')),
+    spacing: require(path.join(process.cwd(), 'Design-Tokens/dist/documentation/spacing.ios.json')),
+    opacity: require(path.join(process.cwd(), 'Design-Tokens/dist/documentation/opacity.ios.json'))
   }
 };
