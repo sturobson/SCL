8daa762cfc2659d48f363c70f6668a07de2bba30
Stu Robson <stu.robson@embl.de>
2020-02-07 07:49:14 +0000

    updates

--- fractal.js
+++ fractal.js
@@ -3,8 +3,8 @@
 'use strict';
 
 /*
- * Dependencies
- */
+* Dependencies
+*/
 
 const fractal    = module.exports = require('@frctl/fractal').create();
 const mandelbrot = require('@frctl/mandelbrot');
@@ -12,23 +12,40 @@ const mandelbrot = require('@frctl/mandelbrot');
 
 fractal.set('project.title', 'Simple Component Library');
 
+const myCustomisedTheme = mandelbrot({
+  skin: "black",
+  "panels": ["html", "resources", "notes"],
+  "styles": [
+    "default",
+    "/theme/theme.css"
+  ]
+  // any other theme configuration values here
+});
+
+
+
 /* Configure components */
 
 fractal.components.set('title', 'Pattern Library');
-fractal.components.set('path', `${__dirname}/patterns`);
+fractal.components.set('path', `${__dirname}/components`);
 fractal.components.set('default.preview', `@preview`);
+fractal.components.set('ext', '.njk'); // look for files with a .nunj file extension
+fractal.components.engine('@frctl/nunjucks'); /* set as the default template engine for components */
 
 /* Configure docs */
 
 fractal.docs.set('path', `${__dirname}/docs`);
 fractal.docs.set('indexLabel', 'Welcome');
-fractal.web.set('builder.dest', __dirname + '/live');
+fractal.docs.set('ext', '.njk'); // look for files with a .njk file extension
+fractal.docs.engine('@frctl/nunjucks'); /* you can also use the same instance for documentation, if you like! */
 
 /* Configure web */
 
+fractal.web.set('builder.dest', __dirname + '/live');
 fractal.web.set('static.path', `${__dirname}/public`);
 fractal.web.set('server.sync', true);
 fractal.web.set('server.syncOptions', {
-    open: true,
-    browser: 'default'
+  open: true,
+  browser: 'default'
 });
+fractal.web.theme(myCustomisedTheme);
--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,30 @@
 // -----------------------------------------------------------------------------
 const gulp              = require('gulp');
 
+// Housekeeping
+
+const fractal           = require('./fractal.js');
+const logger            = fractal.cli.console;
+const path              = require('path');
+const rename            = require("gulp-rename");
+const shell             = require('gulp-shell');
+
 // Sass and CSS Stuff
-const sass              = require('gulp-sass');
-const sassGlob          = require('gulp-sass-glob');
-const autoprefixer      = require('gulp-autoprefixer');
+const sass              = require('gulp-dart-sass');
 const notify            = require("gulp-notify");
 
 // JS Things
 const concat            = require('gulp-concat');
 
+// Design Tokens
+const theoG             = require('gulp-theo');
+const theo              = require('theo');
+const componentPath     = path.resolve(__dirname, 'src' );
 
 // Local Server Stuff
 const browserSync       = require('browser-sync').create();
 const reload            = browserSync.reload;
-const connect           = require('gulp-connect');
-const backstopjs        = require('backstopjs');
 
-// Housekeeping
-
-const fractal           = require('./fractal.js');
-const logger            = fractal.cli.console;
 
 
 
@@ -32,9 +36,167 @@ const logger            = fractal.cli.console;
 // Configuration
 // -----------------------------------------------------------------------------
 
-// Sass and CSS Configarables
 
-const autoprefixerOptions = { browsers: ['last 2 versions', '> 5%', 'Firefox ESR'] };
+// -----------------------------------------------------------------------------
+// Design Tokens
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// Design Tokens Template
+
+
+const theoGeneratedFileWarning = `// This file has been dynamically generated from design tokens
+// Please do NOT edit directly.`;
+const theoSourceTokenLocation = `// Source: {{relative "${ componentPath }" meta.file}}`;
+
+const theoGeneratedSassTemplate = `${theoGeneratedFileWarning}
+${theoSourceTokenLocation}
+
+{{#each props as |prop|}}
+{{#if prop.comment}}
+{{{trimLeft (indent (comment (trim prop.comment)))}}}
+{{/if}}
+\${{prop.name}}: {{#eq prop.type "string"}}"{{/eq}}{{{prop.value}}}{{#eq prop.type "string"}}"{{/eq}} !default;
+{{/each}}
+`;
+
+const theoGeneratedMapTemplate = `${theoGeneratedFileWarning}
+
+${theoSourceTokenLocation}
+
+\${{stem meta.file}}-map: (
+{{#each props as |prop|}}
+  {{#if prop.comment}}
+  {{{trimLeft (indent (comment (trim prop.comment)))}}}
+  {{/if}}
+  '{{prop.name}}': ({{#eq prop.type "string"}}"{{/eq}}{{{prop.value}}}{{#eq prop.type "string"}}"{{/eq}}),
+{{/each}}
+);
+`;
+
+const theoGeneratedPropertiesTemplate = `${theoGeneratedFileWarning}
+
+${theoSourceTokenLocation}
+
+:root {
+  {{#each props as |prop|}}
+  {{#if prop.comment}}
+  {{{trimLeft (indent (comment (trim prop.comment)))}}}
+  {{/if}}
+  --{{prop.name}}: {{#eq prop.type "string"}}"{{/eq}}{{{prop.value}}}{{#eq prop.type "string"}}"{{/eq}};
+{{/each}}
+}
+`;
+
+// -----------------------------------------------------------------------------
+// Design Token Formats
+
+theo.registerFormat( "scss",`${theoGeneratedSassTemplate}`);
+theo.registerFormat( "map.scss",`${theoGeneratedMapTemplate}`);
+theo.registerFormat( "custom-properties.scss",`${theoGeneratedPropertiesTemplate}`);
+theo.registerFormat('typography-map', result => {
+  let { category, type } = result
+    .get('props')
+    .first()
+    .toJS();
+  return `${theoGeneratedFileWarning}
+// Source: ${path.basename(result.getIn(['meta', 'file']))}
+$${category}-map: (
+${result
+  .get('props')
+  .map(
+  prop => `
+  '${prop.get("name")}': (
+    'font-size': ${prop.getIn(["value", "fontSize"])},
+    'font-weight': ${prop.getIn(["value", "fontWeight"])},
+    'line-height': ${prop.getIn(["value", "lineHeight"])}
+  ),`
+  )
+  .sort()
+  .join('\n')}
+);
+  `;
+});
+
+// -----------------------------------------------------------------------------
+// Design Token Transforms
+
+theo.registerTransform("web", ["color/hex"]);
+
+
+// -----------------------------------------------------------------------------
+// Design Token Tasks
+
+
+gulp.task('tokens:variables', () =>
+  gulp.src('./design-tokens/components/*.yml')
+    .pipe(theoG({
+      transform: { type: 'web' },
+      format: { type: 'scss' }
+    }))
+    .pipe(rename(function (path) {
+       path.dirname += "/"+path.basename;
+       path.extname = ".variables.scss";
+   }))
+   .pipe(gulp.dest('./components'))
+);
+
+gulp.task('tokens:typographic-scale', () =>
+  gulp.src('./design-tokens/global/typography.yml')
+    .pipe(theoG({
+      transform: { type: 'web' },
+      format: { type: 'typography-map' }
+    }))
+    .pipe(rename(function (path) {
+      path.extname = ".map.scss";
+    }))
+    .pipe(gulp.dest('./design-tokens/dist/sass/maps'))
+);
+
+gulp.task('tokens:globalVariables', () =>
+  gulp.src(['./design-tokens/global/*.yml', '!./design-tokens/global/typography.yml'])
+    .pipe(theoG({
+      transform: { type: 'web' },
+      format: { type: 'scss' }
+    }))
+    .pipe(rename(function (path) {
+       path.extname = ".variables.scss";
+   }))
+   .pipe(gulp.dest('./design-tokens/dist/sass/variables'))
+);
+
+gulp.task('tokens:documentation', () =>
+  gulp.src(['./design-tokens/global/*.yml'])
+    .pipe(theoG({
+      transform: { type: 'web', includeMeta: true },
+      format: { type: 'ios.json' }
+    }))
+    .pipe(gulp.dest('./design-tokens/dist/documentation'))
+);
+
+gulp.task('tokens:maps', () =>
+  gulp.src(['./design-tokens/global/*.yml', '!./design-tokens/global/typography.yml', '!./design-tokens/global/font-family.yml'])
+    .pipe(theoG({
+      transform: { type: 'web' },
+      format: { type: 'map.scss' }
+    }))
+    .pipe(gulp.dest('./design-tokens/dist/sass/maps'))
+);
+
+gulp.task('tokens:props', () =>
+  gulp.src(['./design-tokens/global/*.yml', '!./design-tokens/global/typography.yml'])
+    .pipe(theoG({
+      transform: { type: 'web' },
+      format: { type: 'custom-properties.scss' }
+    }))
+    .pipe(gulp.dest('./design-tokens/dist/sass/custom-properties'))
+);
+
+
+gulp.task('tokens', gulp.parallel(
+  'tokens:maps', 'tokens:variables', 'tokens:documentation', 'tokens:props', 'tokens:globalVariables', 'tokens:typographic-scale'
+));
+
 
 // -----------------------------------------------------------------------------
 // Sass and CSS Tasks
@@ -42,12 +204,11 @@ const autoprefixerOptions = { browsers: ['last 2 versions', '> 5%', 'Firefox ESR
 
 gulp.task('css', function() {
   return gulp.src('./assets/scss/styles.scss')
-  .pipe(sassGlob())
   .pipe(sass({
+    includePaths: [ path.resolve(__dirname, 'components') ],
     sourcemap: true,
-    sourcemapPath: './patterns/',
+    sourcemapPath: './components/',
   })).on('error', notify.onError(function (error) {return "Problem file : " + error.message;}))
-  .pipe(autoprefixer(autoprefixerOptions))
   .pipe(browserSync.stream())
   .pipe(gulp.dest('./public/css'));
 });
@@ -57,7 +218,7 @@ gulp.task('css', function() {
 // -----------------------------------------------------------------------------
 
 gulp.task('scripts', function() {
-  return gulp.src(['./patterns/**/*.js'])
+  return gulp.src(['./components/**/*.js'])
   .pipe(concat('scripts.js'))
   .pipe(gulp.dest('./public/javascript/'));
 });
@@ -85,51 +246,35 @@ gulp.task('frctlBuild', function () {
   });
 });
 
-// -----------------------------------------------------------------------------
-//  Visual Regression Tests
-// -----------------------------------------------------------------------------
-
-var backstopConfig = {
-  //Config file location
-  config: './backstopConfig.js'
-}
-
-gulp.task('backstop_reference', () => backstopjs('reference', backstopConfig));
-gulp.task('backstop_test', () => backstopjs('test', backstopConfig));
-
-gulp.task('tests', function(done) {
-  connect.server({
-    port: 8888
-  });
-  done();
-});
-gulp.task('testdone', function(done) {
-  connect.serverClose();
-  done();
-});
 
 // -----------------------------------------------------------------------------
 //  Watch Tasks
 // -----------------------------------------------------------------------------
 
 gulp.task('watchCSS', function(done) {
-  gulp.watch('./dev/assets/**/*.scss', gulp.series('css')).on('change', reload);
+  gulp.watch(['./assets/**/*.scss', './components/**/*.scss'], gulp.series('css')).on('change', reload);
   done();
 });
 
 gulp.task('watchJS', function(done) {
-  gulp.watch('./patterns/blocks/**/*.js', gulp.series('scripts')).on('change', reload);
+  gulp.watch('./components/blocks/**/*.js', gulp.series('scripts')).on('change', reload);
   done();
 });
+
+gulp.task('watchTokens', function(done) {
+  gulp.watch(['./design-tokens/theme/*.alias.yml', './design-tokens/components/*.yml'], gulp.series('tokens:variables')).on('change', reload);
+  done();
+});
+
 
 // -----------------------------------------------------------------------------
 // Default Tasks
 // -----------------------------------------------------------------------------
 
+gulp.task('watch', gulp.parallel('watchCSS', 'watchJS', 'watchTokens'));
 
-gulp.task('watch', gulp.parallel('watchCSS', 'watchJS'));
+gulp.task('dev', gulp.series('css', 'frctlStart', 'watch'));
 
-gulp.task('dev', gulp.parallel('frctlStart', 'css', 'watch'));
+gulp.task('build', gulp.series('css', 'frctlBuild'));
 
-gulp.task('vizres-setup', gulp.series('tests', 'css', 'backstop_reference', 'testdone'));
-gulp.task('vizres-test', gulp.series('tests', 'css', 'backstop_test', 'testdone'));
+gulp.task('create-component', shell.task( ['yo ./tools/component-generator'] ));
